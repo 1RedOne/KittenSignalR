2b3751c71e44295fb2fbb2b6cbb2ca74926887bb
Stephen Owen <1redone>
2019-11-30 09:56:37 -0500

    working kind of but fiel upload posts to wrong endpoint

---
+++ KittenSignalR/Controllers/HomeController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using KittenSignalR.Models;
+using Microsoft.AspNetCore.Http;
+using System.IO;
+
+namespace KittenSignalR.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public JsonResult PostTest()
+        {
+            return new JsonResult(new {Result = "Yes!" }
+            );
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> OnPostUploadAsync(List<IFormFile> files)
+        {
+            long size = files.Sum(f => f.Length);
+
+            foreach (var formFile in files)
+            {
+                if (formFile.Length > 0)
+                {
+                    var filePath = Path.GetTempFileName();
+
+                    using (var stream = System.IO.File.Create(filePath))
+                    {
+                        await formFile.CopyToAsync(stream);
+                    }
+                }
+            }
+
+            // Process uploaded files
+            // Don't rely on or trust the FileName property without validation.
+
+            return Ok(new { count = files.Count, size });
+        }
+        
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ KittenSignalR/todo.md
@@ -0,0 +1,30 @@
+## Whats this?
+
+Test project to upload a zip file and then return results back to use immediately over SingalR while we process the zip file
+
+## To Do 
+
+* Add sync file uplaod on bootstrap button click
+
+* get token to send message back to just this user 
+
+* randomize value for file completion based on bunch of files by length
+
+* when parsing each file, send back 'Processing File X of Y', add a progress bar 
+
+## In Progress
+
+🤔- seems to be posting to the wrong endpoint :( 
+
+* Add file upload endpoint
+
+* Add file upload to Index
+
+
+## To Done
+
+✔ - Scaffold button click to AJAX Post
+
+✔ - Signal R working
+
+✔ - Add Signal R
