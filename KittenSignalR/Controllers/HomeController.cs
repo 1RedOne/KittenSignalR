b912fd704270c4a52666985c78cc4e79b014424f
1RedOne <Stephen@foxdeploy.com>
2019-12-11 11:39:51 -0500

    now can send message back to clients!

--- KittenSignalR/Controllers/HomeController.cs
+++ KittenSignalR/Controllers/HomeController.cs
@@ -8,16 +8,23 @@ using Microsoft.Extensions.Logging;
 using KittenSignalR.Models;
 using Microsoft.AspNetCore.Http;
 using System.IO;
+using System.Threading;
+using KittenSignalR.Hubs;
+using Microsoft.AspNetCore.SignalR;
 
 namespace KittenSignalR.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private int executionCount = 0;
+        private Timer _timer;
+        private readonly IHubContext<ChatHub> _hubContext;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IHubContext<ChatHub> hubContext)
         {
             _logger = logger;
+            _hubContext = hubContext;
         }
 
         public IActionResult Index()
@@ -48,27 +55,43 @@ namespace KittenSignalR.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> Upload(List<IFormFile> files)
+        public async Task<IActionResult> Upload(List<IFormFile> files, int FileCount)
         {
             long size = files.Sum(f => f.Length);
-
-            foreach (var formFile in files)
+            string message;
+            for (int i = 1; i <= FileCount; i++)
             {
-                if (formFile.Length > 0)
-                {
-                    var filePath = Path.GetTempFileName();
+                                
+                message = $"Processing file {i} of {FileCount}";
+                //await _hubContext.Clients.All.SendAsync("ReceiveMessage", "Server", message);
+                await _hubContext.Clients.All.SendAsync("ProgressUpdate", "Server", message);
+                Random rnd = new Random();
+                int number = rnd.Next(3, 11);
 
-                    using (var stream = System.IO.File.Create(filePath))
-                    {
-                        await formFile.CopyToAsync(stream);
-                    }
-                }
+                _timer = new Timer(DoWork, null, TimeSpan.Zero,
+                TimeSpan.FromSeconds(1));
+
+                //do something cool
+
+                await Task.Delay(500);
+
+
             }
+            
+            
 
             // Process uploaded files
             // Don't rely on or trust the FileName property without validation.
 
             return Ok(new { count = files.Count, size });
+        }
+
+        private void DoWork(object state)
+        {
+            executionCount++;
+
+            _logger.LogInformation(
+                "Timed Hosted Service is working. Count: {Count}", executionCount);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
