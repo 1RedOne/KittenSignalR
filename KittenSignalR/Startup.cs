1db2ef5583319d962e2fe2698c74018a249e9686
1RedOne <stephen@foxdeploy.com>
2020-03-16 11:43:11 -0400

    adding docker support

--- KittenSignalR/Startup.cs
+++ KittenSignalR/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using KittenSignalR.Hubs;
+using Microsoft.Extensions.FileProviders;
+using System.IO;
 
 namespace KittenSignalR
 {
@@ -43,6 +45,7 @@ namespace KittenSignalR
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+            
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
